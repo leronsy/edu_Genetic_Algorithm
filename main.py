f04c18abf769ee6d70ed28bd5583e1c6c074b1b8
Leronsy <iminnominate@gmail.com>
2018-05-21 12:26:31 +0300

    It's print something

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.datasets import load_boston
 def main():
     features, target = load_boston(return_X_y=True)
     world = World(features, target)
-    print(world)
+    world.evolve()
 
 if  __name__ ==  "__main__" :
     main()
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import random
-# import math
+import math
 import numpy as np
 import random
 import operator
@@ -27,13 +27,14 @@ class Individ:
                 self.chromosome[i] = 1 - gen
             else:
                 self.chromosome[i] = gen
+        return self
 
     def cross(self, parent1, parent2):
-        for gen1, gen2 in parent1, parent2:
+        for i in range(len(self.chromosome)):
             if random.randint(0, 1):
-                self.chromosome.append(gen1)
+                self.chromosome.append(parent1.chromosome[i])
             else:
-                self.chromosome.append(gen2)
+                self.chromosome.append(parent2.chromosome[i])
 
     def generate_chromosome(self, length):
         self.chromosome = [random.randint(0, 1) for _ in range(length)]
@@ -51,67 +52,81 @@ class Individ:
 
 class Population:
 
-    def __init__(self, size, chromosome_length, estimator, features, target,
+    def __init__(self, size, chromosome_length, estimator, features, target, classifier,
                  individ_set_initial=None):
         self.features = features
         self.target = target
         self.size = size
         self.estimator = estimator
-
+        self.classifier = classifier
         self.chromosome_length = chromosome_length
         if not individ_set_initial:
-            self.individ_set_initial = dict()
-            for key in range(self.size):
+            self.individ_set_initial = list()
+            for i in range(self.size):
                 individ = Individ(chromosome_length)
                 fitness = self.fitness(individ.used_features())
-                self.individ_set_initial[key] = [individ, fitness]
+                self.individ_set_initial.append([i, individ, fitness])
+        else:
+            self.individ_set_initial=individ_set_initial
 
     def crossing(self, multiplier=2):
         parent1, parent2 = self.find_best_parents()
-        self.individ_set_extended = dict()
-        for key in range(self.size * multiplier):
+        self.individ_set_extended = list()
+        for i in range(self.size * multiplier):
             individ = Individ(self.chromosome_length, parent1, parent2)
             fitness = self.fitness(individ.used_features())
-            self.individ_set_extended[key] = [individ, fitness]
+            self.individ_set_extended.append([i, individ, fitness])
 
     def mutation(self, probability):
-        self.individ_set_mutated = dict()
-        for key, value in self.individ_set_extended.items():
-            individ = value[0].mutate(probability=probability)
+        self.individ_set_mutated = list()
+        for indiv_list in self.individ_set_extended:
+            i = indiv_list[0]
+            individ = indiv_list[1].mutate(probability=probability)
             fitness = self.fitness(individ.used_features())
-            self.individ_set_mutated[key] = [individ, fitness]
+            self.individ_set_mutated.append([i, individ, fitness])
 
-    def selection(self, selection_limit=None, mutation_probability=0.2, crossing_multiplier=2,new_size=None):
+    def selection(self, selection_limit=None, mutation_probability=0.2, crossing_multiplier=2,
+                  new_size=None):
         if not selection_limit:
             selection_limit = self.size
         if not new_size:
             new_size = self.size
         self.crossing(multiplier=crossing_multiplier)
         self.mutation(probability=mutation_probability)
-        self.individ_set_mutated = self.sort(self.individ_set_mutated)
+        individ_set_future = self.sort(self.individ_set_mutated)[:selection_limit]
 
-        individ_set_future = {key:self.individ_set_mutated[key] for key in range(new_size) if key in self.individ_set_mutated}
         return Population(size=new_size, chromosome_length=self.chromosome_length,
                           estimator=self.estimator, features=self.features, target=self.target,
-                          individ_set_initial=individ_set_future)
+                          individ_set_initial=individ_set_future,classifier=self.classifier)
 
-    def sort(self, population=None,cut=None):
-        return sorted(self.individ_set_mutated.keys(),
-                                          key=lambda x: self.individ_set_mutated[x][1],
-                                          reverse=True)
+    def sort(self, population):
+        return sorted(population, key=lambda ind: ind[2], reverse=True)
 
     def fitness(self, features_list):
-        return self.estimator(self.features[:, features_list], self.target)
+        if features_list:
+            self.classifier.fit(self.features[:,features_list],self.target)
+            predicted = self.classifier.predict(self.features[:,features_list])
+            estimation = self.estimator(predicted, self.target)
+        else:
+            estimation=float('Inf')
+        return estimation
 
     def find_best_parents(self):
-        search_field=self.individ_set_initial
-        self.sort(search_field)
-        i1 = mse_sorted[0][0]
-        i2 = mse_sorted[1][0]
-        return self.individ_list[i1], self.individ_list[i2]
+        parent1_list, parent2_list = self.sort(self.individ_set_initial)[:2]
+        parent1 = parent1_list[1]
+        parent2 = parent2_list[1]
+        return parent1, parent2
 
     def get_info(self):
+        for ind in self.individ_set_initial:
+            print(ind[1].chromosome)
         return
+
+    def get_best(self):
+        best_individ_list = self.sort(self.individ_set_initial)[0]
+        individ = best_individ_list[1]
+        fitness = best_individ_list[2]
+        return individ, fitness
 
 
 class World:
@@ -128,21 +143,31 @@ class World:
         self.crossing_multiplier = crossing_multiplier
         self.mutation_probability = mutation_probability
         self.dimension = self.features.shape[1]
-        self.generation_list = list().append(self.next_gen(first=True))
-        self.next_gen(first=True)
+        self.generation_list = list()
+        self.generation_list.append(self.next_gen(first=True))
 
     def evolve(self):
-        return
+        plato = 0
+        while len(self.generation_list) < self.generation_limit:
+            prev_best_individ, prev_best_score = self.generation_list[-1].get_best()
+            self.generation_list.append(self.next_gen())
+            current_best_individ, current_best_score = self.generation_list[-1].get_best()
+            print(prev_best_score,current_best_score)
+            if prev_best_score - current_best_score < EPS:
+                plato += 1
+                if plato > 2:
+                    return current_best_individ
+        return current_best_individ
 
     def next_gen(self, first=False):
-        generation = None
         if first:
-            generation = Population(self.population_size, self.dimension, self.estimator,
-                                    features=self.features, target=self.target)
+            generation = Population(size=self.population_size, chromosome_length=self.dimension, estimator=self.estimator,
+                                    features=self.features, target=self.target, classifier=self.classifier)
         else:
             generation = self.generation_list[-1].selection(
-                mutation_probability=self.mutation_probability,
-                crossing_multiplier=self.crossing_multiplier)
+            mutation_probability=self.mutation_probability,
+            crossing_multiplier=self.crossing_multiplier)
+        return generation
 
     def genetic(self):
         return
