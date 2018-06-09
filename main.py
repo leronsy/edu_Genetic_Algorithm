c83cd7d1875ed4fde44043962f0b3f2e6cbe8c74
Leronsy <iminnominate@gmail.com>
2018-06-09 17:06:02 +0300

    Отладка. Проверка работы на boston

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from models import World
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_boston, load_diabetes, make_regression
 
 
 def main():
-    features, target = load_boston(return_X_y=True)
-    world = World(features, target)
+    # dataset = make_regression(n_samples=100, n_features=100, n_informative=10, n_targets=1)
+    #features, target = make_regression(n_samples=1000, n_features=10, n_informative=4, n_targets=1, random_state=1)
+    objects, target = load_boston(return_X_y=True)
+    world = World(objects, target)
     world.evolve()
 
 if  __name__ ==  "__main__" :
--- models.py
+++ models.py
@@ -3,89 +3,96 @@ import math
 import numpy as np
 import random
 import operator
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
-
-EPS = 10 ** (-1)
 
 
 class Individ:
 
-    def __init__(self, length=0, parent1=None, parent2=None):
+    def __init__(self, length, parent1=None, parent2=None, chromosome=None):
 
-        self.chromosome = list()
-
-        if parent1 and parent2:
-            self.cross(parent1, parent2)
+        if chromosome:
+            self.chromosome = chromosome
         else:
-            self.generate_chromosome(length)
+            self.chromosome = list()
 
+            if parent1 and parent2:
+                self.cross(parent1, parent2)
+            else:
+                self.generate(length)
+
+    # TODO: можно добавить список генов для мутации (по умолчанию - None)
     def mutate(self, probability):
         for i, gen in enumerate(self.chromosome):
             if np.random.choice([True, False], p=[probability, 1 - probability]):
                 self.chromosome[i] = 1 - gen
             else:
                 self.chromosome[i] = gen
-        return self
+        return Individ(length=len(self.chromosome), chromosome=self.chromosome)
 
     def cross(self, parent1, parent2):
-        for i in range(len(self.chromosome)):
+        for i in range(len(parent1.chromosome)):
             if random.randint(0, 1):
                 self.chromosome.append(parent1.chromosome[i])
             else:
                 self.chromosome.append(parent2.chromosome[i])
 
-    def generate_chromosome(self, length):
+    def generate(self, length):
         self.chromosome = [random.randint(0, 1) for _ in range(length)]
 
-    def info(self):
-        print(self.chromosome, '\n')
-
-    def used_features(self):
-        used_features = list()
+    def used_genes(self):
+        genes = list()
         for i, f in enumerate(self.chromosome):
             if f:
-                used_features.append(i)
-        return used_features
+                genes.append(i)
+        return genes
+
+    def __str__(self):
+        # print('[', end=' ')
+        # for gen in self.chromosome:
+        #     print(gen, end=' ')
+        # print(']\n')
+        return str(self.chromosome)
 
 
 class Population:
 
-    def __init__(self, size, chromosome_length, estimator, features, target, classifier,
+    def __init__(self, size, chromosome_length, estimator, objects, target, classifier,
                  individ_set_initial=None):
-        self.features = features
+        self.objects = objects
         self.target = target
-        self.size = size
+
         self.estimator = estimator
         self.classifier = classifier
+
+        self.size = size
         self.chromosome_length = chromosome_length
         if not individ_set_initial:
             self.individ_set_initial = list()
             for i in range(self.size):
                 individ = Individ(chromosome_length)
-                fitness = self.fitness(individ.used_features())
+                fitness = self.fitness(individ.used_genes())
                 self.individ_set_initial.append([i, individ, fitness])
         else:
-            self.individ_set_initial=individ_set_initial
+            self.individ_set_initial = individ_set_initial
+        self.individ_set_spawned = list()
+        self.individ_set_mutated = list()
 
     def crossing(self, multiplier=2):
         parent1, parent2 = self.find_best_parents()
-        self.individ_set_extended = list()
         for i in range(self.size * multiplier):
             individ = Individ(self.chromosome_length, parent1, parent2)
-            fitness = self.fitness(individ.used_features())
-            self.individ_set_extended.append([i, individ, fitness])
+            fitness = self.fitness(individ.used_genes())
+            self.individ_set_spawned.append([i, individ, fitness])
 
     def mutation(self, probability):
-        self.individ_set_mutated = list()
-        for indiv_list in self.individ_set_extended:
+        for indiv_list in self.individ_set_spawned:
             i = indiv_list[0]
             individ = indiv_list[1].mutate(probability=probability)
-            fitness = self.fitness(individ.used_features())
+            fitness = self.fitness(individ.used_genes())
             self.individ_set_mutated.append([i, individ, fitness])
 
-    def selection(self, selection_limit=None, mutation_probability=0.2, crossing_multiplier=2,
+    def selection(self, selection_limit=None, mutation_probability=0.25, crossing_multiplier=2,
                   new_size=None):
         if not selection_limit:
             selection_limit = self.size
@@ -94,21 +101,24 @@ class Population:
         self.crossing(multiplier=crossing_multiplier)
         self.mutation(probability=mutation_probability)
         individ_set_future = self.sort(self.individ_set_mutated)[:selection_limit]
+        for num, ind in enumerate(individ_set_future):
+            individ_set_future[num][0] = num
 
         return Population(size=new_size, chromosome_length=self.chromosome_length,
-                          estimator=self.estimator, features=self.features, target=self.target,
-                          individ_set_initial=individ_set_future,classifier=self.classifier)
+                          estimator=self.estimator, objects=self.objects, target=self.target,
+                          individ_set_initial=individ_set_future, classifier=self.classifier)
 
+    # TODO: добавить второй параметр сортировки - количество активных генов itertools?
     def sort(self, population):
-        return sorted(population, key=lambda ind: ind[2], reverse=True)
+        return sorted(population, key=lambda ind: ind[2])
 
-    def fitness(self, features_list):
-        if features_list:
-            self.classifier.fit(self.features[:,features_list],self.target)
-            predicted = self.classifier.predict(self.features[:,features_list])
+    def fitness(self, used_genes):
+        if used_genes:
+            self.classifier.fit(self.objects[:, used_genes], self.target)
+            predicted = self.classifier.predict(self.objects[:, used_genes])
             estimation = self.estimator(predicted, self.target)
         else:
-            estimation=float('Inf')
+            estimation = float('Inf')
         return estimation
 
     def find_best_parents(self):
@@ -117,10 +127,10 @@ class Population:
         parent2 = parent2_list[1]
         return parent1, parent2
 
-    def get_info(self):
-        for ind in self.individ_set_initial:
-            print(ind[1].chromosome)
-        return
+    # def get_info(self):
+    #     for ind in self.individ_set_initial:
+    #         ind[1].print_individ()
+    #     return
 
     def get_best(self):
         best_individ_list = self.sort(self.individ_set_initial)[0]
@@ -131,43 +141,46 @@ class Population:
 
 class World:
 
-    def __init__(self, features, target, classifier=LinearRegression(),
-                 estimator=mean_squared_error, generation_limit=10,
-                 population_size=20, mutation_probability=0.25, crossing_multiplier=2):
-        self.features = features
-        self.target = target
+    def __init__(self, objects, target, classifier=LinearRegression(),
+                 estimator=mean_squared_error, generation_limit=20,
+                 population_size=30, mutation_probability=0.2, crossing_multiplier=2):
+        self.train_objects = objects
+        self.train_target = target
         self.classifier = classifier
         self.estimator = estimator
         self.generation_limit = generation_limit
         self.population_size = population_size
         self.crossing_multiplier = crossing_multiplier
         self.mutation_probability = mutation_probability
-        self.dimension = self.features.shape[1]
+        self.dimension = self.train_objects.shape[1]
         self.generation_list = list()
         self.generation_list.append(self.next_gen(first=True))
 
-    def evolve(self):
+    def evolve(self, eps=10 ** -3):
         plato = 0
         while len(self.generation_list) < self.generation_limit:
-            prev_best_individ, prev_best_score = self.generation_list[-1].get_best()
+            last_generation = self.generation_list[-1]
+            prev_best_individ, prev_best_score = last_generation.get_best()
             self.generation_list.append(self.next_gen())
-            current_best_individ, current_best_score = self.generation_list[-1].get_best()
-            print(prev_best_score,current_best_score)
-            if prev_best_score - current_best_score < EPS:
+            last_generation = self.generation_list[-1]
+            cur_best_individ, cur_best_score = last_generation.get_best()
+            print(cur_best_individ, cur_best_score)
+            if math.fabs(prev_best_score - cur_best_score) < eps:
                 plato += 1
                 if plato > 2:
-                    return current_best_individ
-        return current_best_individ
+                    break
+
+        # print(cur_best_individ.print_individ())
+        return cur_best_individ
 
     def next_gen(self, first=False):
         if first:
-            generation = Population(size=self.population_size, chromosome_length=self.dimension, estimator=self.estimator,
-                                    features=self.features, target=self.target, classifier=self.classifier)
+            generation = Population(size=self.population_size, chromosome_length=self.dimension,
+                                    estimator=self.estimator,
+                                    objects=self.train_objects, target=self.train_target,
+                                    classifier=self.classifier)
         else:
-            generation = self.generation_list[-1].selection(
-            mutation_probability=self.mutation_probability,
-            crossing_multiplier=self.crossing_multiplier)
+            last_generation = self.generation_list[-1]
+            generation = last_generation.selection(mutation_probability=self.mutation_probability,
+                                                   crossing_multiplier=self.crossing_multiplier)
         return generation
-
-    def genetic(self):
-        return
